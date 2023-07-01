fbfcc66f74d842461dc8309c6f0f798cb9e6503e
Diego Melo <diego_14melo@hotmail.com>
2023-06-30 23:28:31 -0600

    feat: Modelo de listas

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -65,4 +65,9 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function lists()
+    {
+        return $this->belongsToMany(UserList::class)->withTimestamps();
+    }
 }
