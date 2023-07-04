7d4c3b9d766484dc9c6c85763f6b93267c3f8544
Jemima <jemimaalamilla23@gmail.com>
2023-07-03 20:01:33 -0600

    feat: Componentes Nav y Footer

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="bg-grayf py-6 relative">
+        <div class="container mx-auto flex">
+            <div class="flex flex-grow">
+                <a href="/" class="text-white font-kalam text-2xl">((( Echo )))</a>
+            </div>
+
+            <div class="flex justify-between flex-grow">
+                <div>
+                    <p class="text-white lg:mr-10 font-kalam text-2xl">Let your voice be heard!</p>
+                </div>
+
+                <div>
+                    <a href="/" class="text-white lg:mr-10 font-poppins underline">Terms</a>
+                    <a href="/" class="text-white lg:mr-10 font-poppins underline">Privacy</a>
+                    <a href="/" class="text-white lg:mr-10 font-poppins underline">Help</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+
