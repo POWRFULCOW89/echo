a55b6200d93a543f76afedb1b29b630f323ff958
Jemima <jemimaalamilla23@gmail.com>
2023-07-05 02:31:57 -0600

    feat: Vista Editor

--- resources/js/Pages/Editor.vue
+++ resources/js/Pages/Editor.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { defineProps } from 'vue';
 import Editor from '@tinymce/tinymce-vue';
-import { useForm } from '@inertiajs/vue3'
+import {Head, useForm} from '@inertiajs/vue3'
+import NavAutenticado from "@/Components/NavAutenticado.vue";
+import FooterAutenticado from "@/Components/FooterAutenticado.vue";
 
 const props = defineProps({
     post: Object,
@@ -25,21 +27,45 @@ const submit = () => {
 </script>
 
 <template>
-    <form @submit.prevent="submit">
-        <main class="flex flex-col place-items-center p-8">
-            <div class="flex justify-evenly w-full">
-                <div />
-                <input class="border border-gray-500 w-1/2  text-center rounded-lg" v-model="form.title" />
-                <button type="submit" class="bg-blue text-white p-3 rounded" :disabled="form.processing">
-                    Publish
-                </button>
-            </div>
+    <Head title="Editor" />
+    <NavAutenticado/>
 
-            <div class="w-3/4 p-4">
-                <Editor v-model="form.content" api-key="no-api-key" :init="{
+    <main class="font-poppins">
+        <form @submit.prevent="submit">
+            <main class="flex flex-col place-items-center p-8">
+                <div class="flex justify-evenly w-full">
+                    <div />
+                    <input class="border border-gray-300 w-1/2  text-center rounded-lg" v-model="form.title"/>
+                    <button type="submit" class="bg-blue text-white p-2 rounded-lg" :disabled="form.processing">
+                        Publish
+                    </button>
+                </div>
+                <div class="grid grid-cols-2 gap-4 items-center py-10 text-lg">
+                    <div class="col-span-1 grid justify-items-end">
+                        <p>Tags</p>
+                    </div>
+                    <div class="col-span-1">
+                        <input type="text" class="border border-gray-300 rounded-lg" placeholder="Input 1">
+                    </div>
+                    <div class="col-span-1 grid justify-items-end">
+                        <p>Background Image</p>
+                    </div>
+                    <div class="col-span-1 text-xs">
+                        <p class="border border-gray-300 rounded-lg p-2">
+                            <input type="file" id="fileInput" style="display: none;">
+                            <button class="bg-blue text-white px-3 py-1 rounded-lg" onclick="document.getElementById('fileInput').click()">Browse</button>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="w-3/4 p-4">
+                    <Editor v-model="form.content" api-key="no-api-key" :init="{
                     plugins: 'lists link image table code help wordcount'
                 }" />
-            </div>
-        </main>
-    </form>
+                </div>
+            </main>
+        </form>
+    </main>
+
+    <FooterAutenticado/>
 </template>
