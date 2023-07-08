ce0043a269f417f0a25274ac456c7be5b4c2d95e
Diego Melo <diego_14melo@hotmail.com>
2023-07-07 18:27:51 -0600

    fix: Dropdown de nav ya no envía el formulario

--- resources/js/Components/NavAutenticado.vue
+++ resources/js/Components/NavAutenticado.vue
@@ -41,11 +41,11 @@ const logout = () => {
                     <div class="ml-3 relative">
                         <Dropdown align="right" width="48">
                             <template #trigger>
-                                <button v-if="$page.props.jetstream.managesProfilePhotos"
-                                    class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                <div v-if="$page.props.jetstream.managesProfilePhotos"
+                                    class="hover:cursor-pointer flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                     <img class="h-8 w-8 rounded-full object-cover"
                                         :src="$page.props.auth.user.profile_photo_url" :alt="$page.props.auth.user.name">
-                                </button>
+                                </div>
 
                                 <span v-else class="inline-flex rounded-md">
                                     <button type="button"
